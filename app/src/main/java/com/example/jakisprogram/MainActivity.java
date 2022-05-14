736bdda9ee9701b4faf3c60c2c83420544bb5c5c
stani <nowakstas6@gmail.com>
2022-05-14 16:04:14 +0200

    poczatki kodu

--- app/src/main/java/com/example/jakisprogram/MainActivity.java
+++ app/src/main/java/com/example/jakisprogram/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.jakisprogram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    CalendarView kalendarz;
+    TextView tempo, progres;
+    EditText temposym;
+    SeekBar tempsym2;
+    ProgressBar progresssym;
+    Button zacznijsym;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+kalendarz=findViewById(R.id.kalendarz);
+tempo=findViewById(R.id.textView);
+progres=findViewById(R.id.textView2);
+temposym=findViewById(R.id.editTextNumber);
+tempsym2=findViewById(R.id.seekBar);
+progresssym=findViewById(R.id.progressBar);
+zacznijsym=findViewById(R.id.button);
+
     }
 }
