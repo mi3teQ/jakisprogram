71f5adf89d2bb0fa92915af8ef01ebeded015284
stani <nowakstas6@gmail.com>
2022-05-16 19:58:34 +0200

    prawie git

--- app/src/main/java/com/example/jakisprogram/MainActivity.java
+++ app/src/main/java/com/example/jakisprogram/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.jakisprogram;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.util.Pair;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -39,6 +40,7 @@ zacznijsym=findViewById(R.id.button);
 wybierzdate=findViewById(R.id.button);
 wybranadata=findViewById(R.id.textView3);
 
+
         MaterialDatePicker datePicker=MaterialDatePicker.Builder.dateRangePicker().setSelection(Pair.create(MaterialDatePicker.thisMonthInUtcMilliseconds(),MaterialDatePicker.thisMonthInUtcMilliseconds())).build();
 
         wybierzdate.setOnClickListener(new View.OnClickListener() {
@@ -53,5 +55,16 @@ wybranadata=findViewById(R.id.textView3);
                 });
             }
         });
+        zacznijsym.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int value;
+
+                for (value=0;value<100;value=value+Integer.valueOf(String.valueOf(temposym.getText()))) {
+                    progresssym.setProgress(value);
+                }
+            }
+        });
     }
+
 }
